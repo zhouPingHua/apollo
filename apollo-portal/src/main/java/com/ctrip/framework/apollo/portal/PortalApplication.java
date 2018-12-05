1b6ce4e713185dba78796b7c5f9e1fc8e4391066
zhoupinghua@9188.com <zhoupinghua@9188.com>
2018-12-05 14:12:16 +0800

    配置更新

--- apollo-portal/src/main/java/com/ctrip/framework/apollo/portal/PortalApplication.java
+++ apollo-portal/src/main/java/com/ctrip/framework/apollo/portal/PortalApplication.java
@@ -7,12 +7,14 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @EnableAspectJAutoProxy
 @Configuration
 @EnableAutoConfiguration
 @EnableTransactionManagement
+@PropertySource(value = {"classpath:portal.properties"})
 @ComponentScan(basePackageClasses = {ApolloCommonConfig.class,
     PortalApplication.class, PortalOpenApiConfig.class})
 public class PortalApplication {
